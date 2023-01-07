abbcd965969c6e0f6223c2cfa3f654ffdd6376b5
xVernon <mkuba9261@gmail.com>
2023-01-07 15:20:29 +0100

    Added icons and bottom tab bar background color

--- src/AppManager.tsx
+++ src/AppManager.tsx
@@ -6,27 +6,57 @@ import ProfileComponent from './View/Profile/ProfileComponent';
 import SearcherComponent from './View/Searcher/SearcherComponent';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+const Tab = createMaterialBottomTabNavigator();
 
 const AppManager = () => {
-
-    const Tab = createMaterialBottomTabNavigator();
-
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" 
-                    component={HomeComponent} 
+            <Tab.Navigator
+                activeColor="#ffffff"
+                inactiveColor="#f7eb40"
+                barStyle={{ backgroundColor: '#2b2b2b'}}
+            >
+                <Tab.Screen
+                    name="Home"
+                    component={HomeComponent}
                     options={{
-                        tabBarIcon: () => (
-                          <MaterialCommunityIcons name="home-variant-outline" size={26} />
-                        )}}/>
-                <Tab.Screen name="List" component={ListComponent} />
-                <Tab.Screen name="Searcher" component={SearcherComponent} />
-                <Tab.Screen name="Profile" component={ProfileComponent} />
+                        tabBarIcon: ({color}) => (
+                            <MaterialCommunityIcons name="home-variant-outline" color={color} size={28}/>
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="List"
+                    component={ListComponent}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <MaterialCommunityIcons name="view-list-outline" color={color} size={28}/>
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Searcher"
+                    component={SearcherComponent}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <Entypo name="magnifying-glass" color={color} size={28}/>
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileComponent}
+                    options={{
+                        tabBarIcon: ({color}) => (
+                            <MaterialCommunityIcons name="account" color={color} size={28}/>
+                        ),
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
-        );
-
+    );
 }
 
 export default AppManager;
