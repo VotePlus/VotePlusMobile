57c9eebbe0e64ad08b3b1982182d2e6c18b93ce2
Ukson <jankowskilukasz1337@gmail.com>
2023-01-09 21:30:56 +0100

    Change menu for material bottom tabs to bottom tabs

--- src/AppManager.tsx
+++ src/AppManager.tsx
@@ -5,36 +5,38 @@ import ListComponent from './View/List/ListComponent';
 import ProfileComponent from './View/Profile/ProfileComponent';
 import SearcherComponent from './View/Searcher/SearcherComponent';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Entypo from 'react-native-vector-icons/Entypo';
 import colorsConst from './Consts/colors.json';
 import menuConst from './Consts/menu.json';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppManager = () => {
     return (
         <NavigationContainer independent={true}>
             <Tab.Navigator
-                initialRouteName={menuConst.menuHome.name}
-                // shifting={true}
-                labeled={false}
-                activeColor="#ffffff"
-                inactiveColor={colorsConst.yellowColorForIcons}
-                barStyle={{ 
-                    backgroundColor: colorsConst.grayColorForMenu,
-                    maxHeight: 80,
-                    borderTopWidth: 0,
+                initialRouteName={menuConst.menuHome.name}    
+                screenOptions={{
+                    tabBarStyle:{
+                        maxHeight: 100,
+                    },
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarActiveBackgroundColor: colorsConst.grayColorForMenu,
+                    tabBarInactiveBackgroundColor: colorsConst.grayColorForMenu,
+                    tabBarActiveTintColor: '#ffffff',
+                    tabBarInactiveTintColor: colorsConst.yellowColorForIcons,
+                    
                 }}
-                
             >
                 <Tab.Screen
                     name={menuConst.menuHome.name}
                     component={HomeComponent}
                     options={{
-                        tabBarColor: "red",
                         tabBarIcon: ({color}) => (
-                            <MaterialCommunityIcons name="home-variant-outline" color={color} size={28} />
+                            <MaterialCommunityIcons name="home-variant-outline" color={color} size={28}/>
                         ),
                     }}
                 />
@@ -42,7 +44,6 @@ const AppManager = () => {
                     name={menuConst.menuList.name}
                     component={ListComponent}
                     options={{
-                        tabBarColor: "#575757",
                         tabBarIcon: ({color}) => (
                             <MaterialCommunityIcons name="view-list-outline"  color={color} size={28}/>
                         ),
@@ -52,7 +53,6 @@ const AppManager = () => {
                     name={menuConst.menuSearch.name}
                     component={SearcherComponent}
                     options={{
-                        tabBarColor: "#575757",
                         tabBarIcon: ({color}) => (
                             <MaterialCommunityIcons name="movie-search-outline" color={color} size={28}/>
                         ),
@@ -62,7 +62,6 @@ const AppManager = () => {
                     name={menuConst.menuProfile.name}
                     component={ProfileComponent}
                     options={{
-                        tabBarColor: "#575757",
                         tabBarIcon: ({color}) => (
                             <MaterialCommunityIcons name="account" color={color} size={28}/>
                         ),
